7e5d015b79d3b8c85b1444063d57da95171dc6cd
navdeepgill-web <79119745+navdeepgill-web@users.noreply.github.com>
2021-02-15 23:44:12 -0500

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { UploadFileComponent } from './upload-file/upload-file.component';
-import { ViewSalesComponent } from './view-sales/view-sales.component';
-import { UploadFileService } from './upload-file/services/upload-file.service';
-import { UtilsService } from './shared/services/utils.service';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { MatCardModule, MatDividerModule, MatPaginatorModule, MatTableModule, MatToolbarModule } from '@angular/material';
-import 'hammerjs';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./home/home.component";
+import { UploadFileComponent } from "./upload-file/upload-file.component";
+import { ViewSalesComponent } from "./view-sales/view-sales.component";
+import { UploadFileService } from "./upload-file/services/upload-file.service";
+import { UtilsService } from "./shared/services/utils.service";
+import { NoopAnimationsModule } from "@angular/platform-browser/animations";
+import "hammerjs";
+import { MatTableModule } from "@angular/material/table";
+import { MatPaginatorModule } from "@angular/material/paginator";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatCardModule } from "@angular/material/card";
+import { MatDividerModule } from "@angular/material/divider";
 
 @NgModule({
   declarations: [
@@ -20,7 +24,7 @@ import 'hammerjs';
     ViewSalesComponent
   ],
   imports: [
-    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+    BrowserModule.withServerTransition({ appId: "ng-cli-universal" }),
     HttpClientModule,
     FormsModule,
     NoopAnimationsModule,
@@ -30,8 +34,7 @@ import 'hammerjs';
     MatCardModule,
     MatDividerModule
   ],
-  providers: [UploadFileService,
-    UtilsService],
+  providers: [UploadFileService, UtilsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
